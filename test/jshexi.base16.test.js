f7c7ff65d395549a079e864e93bbaafdd6eabbf1
Mykola Bubelich <thesimj@users.noreply.github.com>
2017-01-27 14:35:18 +0100

    Update test to standard js code style

--- test/jshexi.base16.test.js
+++ test/jshexi.base16.test.js
@@ -3,58 +3,58 @@
  * 2017-01-19.
  */
 
-import test from "tape";
-import JSHexi from "../lib/jshexi";
+import test from 'tape'
+import JSHexi from '../src/jshexi'
 
 const verter = {
   in: [
-    "Hello world",
-    "Hello world (｡◕‿◕｡)"
+    'Hello world',
+    'Hello world (｡◕‿◕｡)'
   ],
   out: [
-    "48656c6c6f20776f726c64",
-    "00480065006c006c006f00200077006f0072006c006400200028ff6125d5203f25d5ff610029"
+    '48656c6c6f20776f726c64',
+    '00480065006c006c006f00200077006f0072006c006400200028ff6125d5203f25d5ff610029'
   ]
-};
+}
 
 /**
  * Text to Base16 (hex)
  */
 
 // ENCODE //
-test("Base16: Non unicode string should be converted to proper base16 string", tape => {
-  const bytes = JSHexi.toBytes(verter.in[0], false);
-  const result = JSHexi.toBase16(bytes);
+test('Base16: Non unicode string should be converted to proper base16 string', tape => {
+  const bytes = JSHexi.toBytes(verter.in[0], false)
+  const result = JSHexi.toBase16(bytes)
 
-  tape.equal(result, verter.out[0]);
+  tape.equal(result, verter.out[0])
 
-  tape.end();
-});
+  tape.end()
+})
 
-test("Base16: Unicode string should be converted to proper base16 string", tape => {
-  const bytes = JSHexi.toBytes(verter.in[1]);
-  const result = JSHexi.toBase16(bytes);
+test('Base16: Unicode string should be converted to proper base16 string', tape => {
+  const bytes = JSHexi.toBytes(verter.in[1])
+  const result = JSHexi.toBase16(bytes)
 
-  tape.equal(result, verter.out[1]);
+  tape.equal(result, verter.out[1])
 
-  tape.end();
-});
+  tape.end()
+})
 
 // DECODE //
-test("Base16: Base16 string should be converted to proper non unicode string", tape => {
-  const bytes = JSHexi.fromBase16(verter.out[0]);
-  const result = JSHexi.fromBytes(bytes, false);
+test('Base16: Base16 string should be converted to proper non unicode string', tape => {
+  const bytes = JSHexi.fromBase16(verter.out[0])
+  const result = JSHexi.fromBytes(bytes, false)
 
-  tape.equal(result, verter.in[0]);
+  tape.equal(result, verter.in[0])
 
-  tape.end();
-});
+  tape.end()
+})
 
-test("Base16: Base16 string should be converted to proper unicode string", tape => {
-  const bytes = JSHexi.fromBase16(verter.out[1]);
-  const result = JSHexi.fromBytes(bytes);
+test('Base16: Base16 string should be converted to proper unicode string', tape => {
+  const bytes = JSHexi.fromBase16(verter.out[1])
+  const result = JSHexi.fromBytes(bytes)
 
-  tape.equal(result, verter.in[1]);
+  tape.equal(result, verter.in[1])
 
-  tape.end();
-});
+  tape.end()
+})
--- test/jshexi.bytes.test.js
+++ test/jshexi.bytes.test.js
@@ -3,65 +3,64 @@
  * 2017-01-19.
  */
 
-import test from "tape";
-import JSHexi from "../lib/jshexi";
+import test from 'tape'
+import JSHexi from '../src/jshexi'
 
 const verter = {
   in: [
-    "Hello world",
-    "Hello world (｡◕‿◕｡)"
+    'Hello world',
+    'Hello world (｡◕‿◕｡)'
   ],
   out: [
     new Uint8Array([72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100]),
     new Uint8Array([0, 72, 0, 101, 0, 108, 0, 108, 0, 111, 0, 32, 0, 119, 0, 111, 0, 114, 0, 108, 0, 100, 0, 32, 0, 40, 255, 97, 37, 213, 32, 63, 37, 213, 255, 97, 0, 41])
   ]
-};
+}
 
 /**
  * Text to bytes
  */
 
 // ENCODE //
-test("Bytes: Non unicode string should be converted to proper byte array", tape => {
-  tape.deepEquals(JSHexi.toBytes(verter.in[0], false), verter.out[0]);
+test('Bytes: Non unicode string should be converted to proper byte array', tape => {
+  tape.deepEquals(JSHexi.toBytes(verter.in[0], false), verter.out[0])
 
-  tape.end();
-});
+  tape.end()
+})
 
-test("Bytes: Unicode string should be converted to proper byte array", tape => {
-  tape.deepEquals(JSHexi.toBytes(verter.in[1], true), verter.out[1]);
+test('Bytes: Unicode string should be converted to proper byte array', tape => {
+  tape.deepEquals(JSHexi.toBytes(verter.in[1], true), verter.out[1])
 
-  tape.end();
-});
+  tape.end()
+})
 
 // DECODE
-test("Bytes: Bytes should be decoded to non unicode string", tape => {
-  tape.equal(JSHexi.fromBytes(verter.out[0], false), verter.in[0]);
+test('Bytes: Bytes should be decoded to non unicode string', tape => {
+  tape.equal(JSHexi.fromBytes(verter.out[0], false), verter.in[0])
 
-  tape.end();
-});
+  tape.end()
+})
 
-test("Bytes: Bytes should be decoded to unicode string", tape => {
-  tape.equal(JSHexi.fromBytes(verter.out[1]), verter.in[1]);
+test('Bytes: Bytes should be decoded to unicode string', tape => {
+  tape.equal(JSHexi.fromBytes(verter.out[1]), verter.in[1])
 
-  tape.end();
-});
+  tape.end()
+})
 
 // ERRORS
-test("Bytes: Convert unicode string to non unicode bytes should throws error", tape => {
+test('Bytes: Convert unicode string to non unicode bytes should throws error', tape => {
   tape.throws(() => {
-    JSHexi.toBytes(verter.in[1], false);
-  }, /Unicode char in data!/);
+    JSHexi.toBytes(verter.in[1], false)
+  }, /Unicode char in data!/)
 
-  tape.end();
-});
+  tape.end()
+})
 
-test("Bytes: Convert unicode string to non unicode bytes should throws error", tape => {
+test('Bytes: Convert unicode string to non unicode bytes should throws error', tape => {
   tape.throws(() => {
-    JSHexi.fromBytes(null, false);
-  }, /Data not byte array!/);
+    JSHexi.fromBytes(null, false)
+  }, /Data not byte array!/)
 
-  tape.end();
-});
-
+  tape.end()
+})
 
