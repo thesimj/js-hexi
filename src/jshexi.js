2d2ec494f1444987f5f510bb3afad7272b637601
Mykola Bubelich <thesimj@users.noreply.github.com>
2017-01-27 14:34:50 +0100

    Update source file. Switch standard js. Write all in ES5

--- src/jshexi.js
+++ src/jshexi.js
@@ -33,302 +33,296 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-/** Base 16 Alphabet **/
-const _JENCB16 = "0123456789abcdef";
-const _JDECB16 = [
-  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-  -1, -1, -1, -1, -1, -1, -1, -1, 0, 1,
-  2, 3, 4, 5, 6, 7, 8, 9, -1, -1, -1, -1,
-  -1, -1, -1, 10, 11, 12, 13, 14, 15, -1,
-  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-  -1, -1, -1, -1, -1, 10, 11, 12, 13, 14, 15
-];
+var JSHexi = (function () {
+  /** Base 16 Alphabet **/
+  var _JENCB16 = '0123456789abcdef'
+  var _JDECB16 = [
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, 0, 1,
+    2, 3, 4, 5, 6, 7, 8, 9, -1, -1, -1, -1,
+    -1, -1, -1, 10, 11, 12, 13, 14, 15, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, 10, 11, 12, 13, 14, 15
+  ]
 
-/** Base 64 Alphabet **/
-const _JENCB64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-const _JDECB64 = [
-  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-  -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, 62, -1, 63, 52, 53, 54,
-  55, 56, 57, 58, 59, 60, 61, -1, -1, -1, 0, -1, -1, -1, 0, 1, 2, 3, 4,
-  5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-  24, 25, -1, -1, -1, -1, 63, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34,
-  35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51
-];
+  /** Base 64 Alphabet **/
+  var _JENCB64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+  var _JDECB64 = [
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, 62, -1, 63, 52, 53, 54,
+    55, 56, 57, 58, 59, 60, 61, -1, -1, -1, 0, -1, -1, -1, 0, 1, 2, 3, 4,
+    5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+    24, 25, -1, -1, -1, -1, 63, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34,
+    35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51
+  ]
 
-/**
- *
- */
-class JSHexi {
-  /**
-   * Decode input string in Base16 to array of byte.
-   *
-   * @param {String} data
-   * @return {Uint8Array}
-   */
-  static fromBase16(data) {
-    JSHexi._checkLength(data.length, 2);
+  return {
+    /**
+     * Decode input string in Base16 to array of byte.
+     *
+     * @param {String} data
+     * @return {Uint8Array}
+     */
+    fromBase16: function (data) {
+      this._checkLength(data.length, 2)
 
-    const result = new Uint8Array(data.length >> 1);
-    let code_a, code_b;
+      var result = new Uint8Array(data.length >> 1)
 
-    for (let i = 0; i < result.length; i++) {
-      code_a = data.charCodeAt(2 * i);
-      code_b = data.charCodeAt(2 * i + 1);
-
-      result[i] = _JDECB16[code_a] << 4 | _JDECB16[code_b];
-    }
-
-    return result;
-  }
-
-  /**
-   * Decode input string in Base64 to array of byte.
-   *
-   * @param {String} data
-   * @return {Uint8Array}
-   */
-  static fromBase64(data) {
-
-    let dlen = data.length;
-    let rlen = dlen * 3 / 4;
-
-    if (dlen % 2 == 0) {
-      if (data[dlen - 1] === "=") {
-        dlen--;
-        rlen--;
+      for (var i = 0; i < result.length; i++) {
+        result[i] = _JDECB16[data.charCodeAt(2 * i)] << 4 | _JDECB16[data.charCodeAt(2 * i + 1)]
       }
 
-      if (data[dlen - 1] === "=") {
-        dlen--;
-        rlen--;
+      return result
+    },
+
+    /**
+     * Decode input string in Base64 to array of byte.
+     *
+     * @param {String} data
+     * @return {Uint8Array}
+     */
+    fromBase64: function (data) {
+      var dlen = data.length
+      var rlen = dlen * 3 / 4
+
+      if (dlen % 2 === 0) {
+        if (data[dlen - 1] === '=') {
+          dlen--
+          rlen--
+        }
+
+        if (data[dlen - 1] === '=') {
+          dlen--
+          rlen--
+        }
       }
-    }
 
-    const result = new Uint8Array(rlen);
-    let dcounter = 0, rcounter = 0, buff, code_a, code_b, code_c, code_d;
+      var result = new Uint8Array(rlen)
+      var dcounter = 0
+      var rcounter = 0
+      var buff = 0
 
-    while (dlen >= 4) {
-
-      code_a = data.charCodeAt(dcounter++);
-      code_b = data.charCodeAt(dcounter++);
-      code_c = data.charCodeAt(dcounter++);
-      code_d = data.charCodeAt(dcounter++);
-
-      buff = _JDECB64[code_a] << 18 |
-        _JDECB64[code_b] << 12 |
-        _JDECB64[code_c] << 6 |
-        _JDECB64[code_d];
-
-      result[rcounter++] = (buff >> 16) & 0xFF;
-      result[rcounter++] = (buff >> 8) & 0xFF;
-      result[rcounter++] = buff & 0xFF;
-
-      dlen -= 4;
-    }
-
-    // Padding stage //
-    switch (rlen - rcounter) {
-      case 2: {
+      while (dlen >= 4) {
         buff = _JDECB64[data.charCodeAt(dcounter++)] << 18 |
           _JDECB64[data.charCodeAt(dcounter++)] << 12 |
-          _JDECB64[data.charCodeAt(dcounter++)] << 6;
+          _JDECB64[data.charCodeAt(dcounter++)] << 6 |
+          _JDECB64[data.charCodeAt(dcounter++)]
 
-        result[rcounter++] = (buff >> 16) & 0xFF;
-        result[rcounter++] = (buff >> 8) & 0xFF;
+        result[rcounter++] = (buff >> 16) & 0xFF
+        result[rcounter++] = (buff >> 8) & 0xFF
+        result[rcounter++] = buff & 0xFF
 
-        break;
+        dlen -= 4
       }
 
-      case 1 : {
-        buff = _JDECB64[data.charCodeAt(dcounter++)] << 18 |
-          _JDECB64[data.charCodeAt(dcounter++)] << 12;
+      // Padding stage //
+      switch (rlen - rcounter) {
+        case 2: {
+          buff = _JDECB64[data.charCodeAt(dcounter++)] << 18 |
+            _JDECB64[data.charCodeAt(dcounter++)] << 12 |
+            _JDECB64[data.charCodeAt(dcounter++)] << 6
 
-        result[rcounter++] = (buff >> 16) & 0xFF;
+          result[rcounter++] = (buff >> 16) & 0xFF
+          result[rcounter++] = (buff >> 8) & 0xFF
 
-        break;
-      }
-    }
+          break
+        }
 
-    return result;
-  }
+        case 1 : {
+          buff = _JDECB64[data.charCodeAt(dcounter++)] << 18 |
+            _JDECB64[data.charCodeAt(dcounter++)] << 12
 
-  /**
-   * Encode input byte array as Base64 string
-   *
-   * @param {Uint8Array} data
-   * @return {String}
-   */
-  static toBase64(data) {
-    JSHexi._checkForUint8Array(data);
+          result[rcounter++] = (buff >> 16) & 0xFF
 
-    let dlen = data.length;
-    let result = "", buff, dcounter = 0;
-
-    while (dlen >= 3) {
-      buff = data[dcounter++] << 16 | data[dcounter++] << 8 | data[dcounter++];
-
-      result += _JENCB64[buff >> 18 & 0x3F];
-      result += _JENCB64[buff >> 12 & 0x3F];
-      result += _JENCB64[buff >> 6 & 0x3F];
-      result += _JENCB64[buff & 0x3F];
-
-      dlen -= 3;
-    }
-
-    // Padding stage //
-    switch (dlen) {
-      case 2 : {
-        buff = data[dcounter++] << 16 | data[dcounter++] << 8;
-        result += _JENCB64[buff >> 18 & 0x3F];
-        result += _JENCB64[buff >> 12 & 0x3F];
-        result += _JENCB64[buff >> 6 & 0x3F];
-        result += "=";
-
-        break;
+          break
+        }
       }
 
-      case 1 : {
-        buff = data[dcounter++] << 16;
-        result += _JENCB64[buff >> 18 & 0x3F];
-        result += _JENCB64[buff >> 12 & 0x3F];
-        result += "==";
+      return result
+    },
 
-        break;
+    /**
+     * Encode input byte array as Base64 string
+     *
+     * @param {Uint8Array} data
+     * @return {String}
+     */
+    toBase64: function (data) {
+      this._checkForUint8Array(data)
+
+      var dlen = data.length
+      var result = ''
+      var buff
+      var dcounter = 0
+
+      while (dlen >= 3) {
+        buff = data[dcounter++] << 16 | data[dcounter++] << 8 | data[dcounter++]
+
+        result += _JENCB64[buff >> 18 & 0x3F]
+        result += _JENCB64[buff >> 12 & 0x3F]
+        result += _JENCB64[buff >> 6 & 0x3F]
+        result += _JENCB64[buff & 0x3F]
+
+        dlen -= 3
       }
-    }
 
-    return result;
-  }
+      // Padding stage //
+      switch (dlen) {
+        case 2 : {
+          buff = data[dcounter++] << 16 | data[dcounter++] << 8
+          result += _JENCB64[buff >> 18 & 0x3F]
+          result += _JENCB64[buff >> 12 & 0x3F]
+          result += _JENCB64[buff >> 6 & 0x3F]
+          result += '='
 
-  /**
-   * Encode input byte array as Base16 string
-   *
-   * @param {Uint8Array} data
-   * @return {String}
-   */
-  static toBase16(data) {
-    JSHexi._checkForUint8Array(data);
+          break
+        }
 
-    let result = "";
+        case 1 : {
+          buff = data[dcounter++] << 16
+          result += _JENCB64[buff >> 18 & 0x3F]
+          result += _JENCB64[buff >> 12 & 0x3F]
+          result += '=='
 
-    for (let i = 0; i < data.length; i++) {
-      result = result.concat(_JENCB16[data[i] >>> 4], _JENCB16[data[i] & 0x0F]);
-    }
-
-    return result;
-  }
-
-  /**
-   * Checks if unicode chars in data string
-   *
-   * @param {String} data
-   * @return {boolean}
-   */
-  static isUnicodeString(data) {
-    for (let i = 0; i < data.length; i++) {
-      if (data.charCodeAt(i) > 0xFF) {
-        return true;
+          break
+        }
       }
-    }
 
-    return false;
-  }
+      return result
+    },
 
-  /**
-   * Convert string data to bytes
-   *
-   * @throws {Error} when data contain unicode char, but unicode flag set to false
-   * @param {String} data
-   * @param {Boolean} unicode
-   * @return {Uint8Array}
-   */
-  static toBytes(data, unicode = true) {
-    JSHexi._checkForUnicode(data, unicode);
+    /**
+     * Encode input byte array as Base16 string
+     *
+     * @param {Uint8Array} data
+     * @return {String}
+     */
+    toBase16: function (data) {
+      this._checkForUint8Array(data)
 
-    const buffer = new Uint8Array(unicode ? data.length << 1 : data.length);
-    let i, code;
+      var result = ''
 
-    for (i = 0; i < data.length; i++) {
-      code = data.charCodeAt(i);
+      for (var i = 0; i < data.length; i++) {
+        result = result.concat(_JENCB16[data[i] >>> 4], _JENCB16[data[i] & 0x0F])
+      }
+
+      return result
+    },
+
+    /**
+     * Checks if unicode chars in data string
+     *
+     * @param {String} data
+     * @return {boolean}
+     */
+    isUnicodeString: function (data) {
+      for (var i = 0; i < data.length; i++) {
+        if (data.charCodeAt(i) > 0xFF) {
+          return true
+        }
+      }
+
+      return false
+    },
+
+    /**
+     * Convert string data to bytes
+     *
+     * @throws {Error} when data contain unicode char, but unicode flag set to false
+     * @param {String} data
+     * @param {Boolean} unicode
+     * @return {Uint8Array}
+     */
+    toBytes: function (data, unicode = true) {
+      this._checkForUnicode(data, unicode)
+
+      var buffer = new Uint8Array(unicode ? data.length << 1 : data.length)
+      var i, code
+
+      for (i = 0; i < data.length; i++) {
+        code = data.charCodeAt(i)
+
+        if (unicode) {
+          buffer[i * 2] = (code >> 8) & 0xFF
+          buffer[i * 2 + 1] = code & 0xFF
+        } else {
+          buffer[i] = code
+        }
+      }
+
+      return buffer
+    },
+
+    /**
+     * Convert bytes to string, can be unicode string
+     *
+     * @param {Uint8Array} data
+     * @param {Boolean} unicode
+     */
+    fromBytes: function (data, unicode = true) {
+      this._checkForUint8Array(data)
+
+      var udata = (unicode) ? new Uint16Array(data.length / 2) : data
 
       if (unicode) {
-        buffer[i * 2] = (code >> 8) & 0xFF;
-        buffer[i * 2 + 1] = code & 0xFF;
-      } else {
-        buffer[i] = code;
+        for (var i = 0; i < udata.length; i++) {
+          udata[i] = ((data[2 * i] << 8) | (data[2 * i + 1])) & 0xFFFF
+        }
+      }
+
+      return String.fromCodePoint(...udata)
+    },
+
+    /**
+     * Check input string for unicode char
+     * If found unicode char, throw error
+     *
+     * @throws {Error}
+     * @param {String} data
+     * @param {Boolean} unicode
+     * @private
+     */
+    _checkForUnicode: function (data, unicode = true) {
+      if (!unicode && this.isUnicodeString(data)) {
+        throw new Error('Unicode char in data!')
+      }
+    },
+
+    /**
+     * Check length by module
+     *
+     * @param length
+     * @param mod
+     * @private
+     */
+    _checkLength: function (length, mod) {
+      if (length % mod !== 0) {
+        throw new Error('Data string has wrong length!')
+      }
+    },
+
+    /**
+     * Check for input if this byte array.
+     * If not, throws Error
+     *
+     * @throws {Error}
+     * @param {*} data
+     * @private
+     */
+    _checkForUint8Array: function (data) {
+      if (!(data instanceof Uint8Array)) {
+        throw new Error('Data not byte array!')
       }
     }
-
-    return buffer;
   }
+})()
 
-  /**
-   * Convert bytes to string, can be unicode string
-   *
-   * @param {Uint8Array} data
-   * @param {Boolean} unicode
-   */
-  static fromBytes(data, unicode = true) {
-    JSHexi._checkForUint8Array(data);
-
-    const udata = (unicode) ? new Uint16Array(data.length / 2) : data;
-
-    if (unicode) {
-      for (let i = 0; i < udata.length; i++) {
-        udata[i] = ((data[2 * i] << 8) | (data[2 * i + 1])) & 0xFFFF;
-      }
-    }
-
-    return String.fromCodePoint(...udata);
-  }
-
-  /**
-   * Check input string for unicode char
-   * If found unicode char, throw error
-   *
-   * @throws {Error}
-   * @param {String} data
-   * @param {Boolean} unicode
-   * @private
-   */
-  static _checkForUnicode(data, unicode = true) {
-    if (!unicode && JSHexi.isUnicodeString(data)) {
-      throw new Error("Unicode char in data!");
-    }
-  }
-
-  /**
-   * Check length by module
-   *
-   * @param length
-   * @param mod
-   * @private
-   */
-  static _checkLength(length, mod) {
-    if (length % mod !== 0) {
-      throw new Error("Data string has wrong length!");
-    }
-  }
-
-  /**
-   * Check for input if this byte array.
-   * If not, throws Error
-   *
-   * @throws {Error}
-   * @param {*} data
-   * @private
-   */
-  static _checkForUint8Array(data) {
-    if (!(data instanceof Uint8Array)) {
-      throw new Error("Data not byte array!");
-    }
-  }
-}
-
-/** Exports **/
-export default JSHexi;
+// Export //
+// if (typeof module !== 'undefined' && module.exports) {
+module.exports = JSHexi
+// }
